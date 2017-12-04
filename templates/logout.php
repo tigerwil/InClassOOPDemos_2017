be53abb65fa2cd8a515adc64cd630b9947e2e3d2
oti_i <oti_i@DESKTOP-OUP9URT>
2017-12-04 08:26:55 -0400

    Added Account and logout pages

---
+++ templates/logout.php
@@ -0,0 +1,19 @@
+<!-- About Page Template Content -->
+<div class="container">
+    <h1 class="mt-4 mb-3">Logout</h1>
+
+    <!-- mwilliams:  breadcrumb navigation -->
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+        <li class="breadcrumb-item active">Logout</li>            
+    </ol>
+    <!-- end breadcrumb -->
+    <h2 class="mt-3 mb-3">Logged Out</h2>
+    <div class="alert alert-primary" role="alert">
+        Thank you for visiting.  You are now logged out! <br>
+        Please come back soon!
+    </div>
+
+
+</div>
+<!-- /.container -->
